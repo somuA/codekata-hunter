4c46c3ecaa1fa66c9864cce81e2693f0529d5c2e
somuA <37236107+somuA@users.noreply.github.com>
2019-02-05 08:54:39 -0800

    Add files via upload

---
+++ day holiday.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      somu
+#
+# Created:     05-02-2019
+# Copyright:   (c) somu 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+list=['Monday','Tuesday','Wednesday','Thursday','Fryday']
+s=input()
+
+if s in list:
+    print ("no")
+else:
+    print("yes")
