1ca5dd35be3ba1114bc62f81fc25a38a2c2890c5
somuA <37236107+somuA@users.noreply.github.com>
2019-02-05 07:48:40 -0800

    Add files via upload

---
+++ reverse with arrow.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      somu
+#
+# Created:     05-02-2019
+# Copyright:   (c) somu 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+n=int(input())
+a=[]
+b=[]
+a= list(map(int, input().split()))
+a.reverse()
+for i in range(0,n):
+    b.append(a[i])
+    b.append("->")
+b.pop(-1)
+print(''.join(map(str, b)))
