fffe21c4af152f745916c414e87012566e285c1c
somuA <37236107+somuA@users.noreply.github.com>
2019-02-07 06:14:13 -0800

    Add files via upload

---
+++ sum of digit with power of total digit.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      somu
+#
+# Created:     05-02-2019
+# Copyright:   (c) somu 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+n=int(input())
+b=[]
+sum=0
+digits = [int(x) for x in str(n)]
+r=len(digits)
+for i in range(0,r):
+    sum=sum+digits[i]**r
+print (sum)
+
