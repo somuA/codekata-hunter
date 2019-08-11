d399a5ab270b04a4c048ff4cf7a4c510204bf225
somuA <37236107+somuA@users.noreply.github.com>
2019-08-11 15:02:15 +0530

    Create email checking.py

---
+++ email checking.py
@@ -0,0 +1,9 @@
+n2=input()
+a=n2.count('.')
+b=n2.count('@')
+n=list(n2.split('@'))
+n1=n[1].split('.')
+if len(n[0])>=3 and n1[1]=='com' and len(n1[0])>=4 and b==1 and a==1 :
+    print('YES')
+else:
+    print("NO")
