3cd8443ea6126744720ad535a41912e3dc1aa900
somuA <37236107+somuA@users.noreply.github.com>
2019-08-11 15:06:04 +0530

    Update email checking.py

--- email checking.py
+++ email checking.py
@@ -3,7 +3,7 @@ a=n2.count('.')
 b=n2.count('@')
 n=list(n2.split('@'))
 n1=n[1].split('.')
-if len(n[0])>=3 and n1[1]=='com' and len(n1[0])>=4 and b==1 and a==1 :
+if len(n[0])>=3 and n1[1]=='com' and len(n1[0])<6 and b==1 and a==1 :
     print('YES')
 else:
     print("NO")
