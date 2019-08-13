8ed1df3e7abc911a0cff2445e2735c5dacd637b0
somuA <37236107+somuA@users.noreply.github.com>
2019-08-13 20:50:36 +0530

    Update padding.c

--- padding.c
+++ padding.c
@@ -5,7 +5,15 @@ char ch;
 int a,i;
 while(scanf("%c%d",&ch,&a)>0)
 {
+    if (a==1)
+    {
 for(i=0;i<a;i++)
+printf("%c%d",ch,a);
+    }
+else{
+        for(i=0;i<a;i++)
 printf("%c",ch);
+
+}
 }
 }
